acd5b0192df171b046a575ed10a305de6b3fb4bf
Brian Luft <brian@electroly.com>
2014-02-02 23:22:43 -0600

    Revert "Replace error check with 60 second timeout."
    This reverts commit 1e38539647cc7de49177a6ea3e51c9abff69cb6b.

--- v2/waitForEvent.php
+++ v2/waitForEvent.php
@@ -20,11 +20,11 @@ $filePath = '/mnt/ssd/ChattyIndex/LastEventID';
 $eventsFilePath = '/mnt/ssd/ChattyIndex/LastEvents';
 $lastId = nsc_getArg('lastEventId', 'INT');
 
-$attempts = 0;
+if ($lastId > intval(file_get_contents($filePath)))
+   nsc_die('NSC_ARGUMENT', 'lastEventId is higher than any existing event.');
 
-while ($attempts < 60 && intval(file_get_contents($filePath)) <= $lastId)
+while (intval(file_get_contents($filePath)) <= $lastId)
 {
-   $attempts++;
    sleep(1);
    # I know, right?  Gets the job done though.  Programming is hard.
 }
