29aefe14708d0e39fcd01dddc85e085bb8f35423
Brian Luft <brian@electroly.com>
2014-03-30 15:55:57 -0500

    Use POST for ShackLOL reports.

--- service/services/LOLService.php
+++ service/services/LOLService.php
@@ -7,11 +7,24 @@ class LOLService
       $post_id   = intval($post_id);
       $tag       = urlencode($tag);
       $addr      = $_SERVER['REMOTE_ADDR'];
-      		
       $args      = "who=$username&what=$post_id&tag=$tag&version=-1&addr=$addr";
-
       $url       = 'http://lmnopc.com/greasemonkey/shacklol/report.php';
-      $result    = file_get_contents($url . '?' . $args);      
+
+      $curl = curl_init();
+      curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($curl, CURLOPT_HEADER, true);
+      curl_setopt($curl, CURLOPT_ENCODING, 'gzip');
+      curl_setopt($curl, CURLOPT_USERAGENT, 'WinChatty API');
+      curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 30);
+      curl_setopt($curl, CURLOPT_TIMEOUT, 30);
+
+      curl_setopt($curl, CURLOPT_URL, $url);
+      curl_setopt($curl, CURLOPT_POST, true);
+      curl_setopt($curl, CURLOPT_POSTFIELDS, $args);
+      $result = curl_exec($curl);
+      
+      curl_close($curl);
       
       $expected  = "ok $tag$post_id";
       if (strpos($result, $expected) === false)
