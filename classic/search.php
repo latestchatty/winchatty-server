82c2b03f44b1cad4bdb42eac929122b96f87dd97
Brian Luft <brian@electroly.com>
2014-06-10 23:00:44 -0500

    Fix iOS comment searches containing brackets.

--- classic/search.php
+++ classic/search.php
@@ -1,11 +1,8 @@
 <?
 require_once '../include/Global.php';
 
-$author = urlencode(isset($_REQUEST['author']) ? $_REQUEST['author'] : '');
-$parent = urlencode(isset($_REQUEST['parent_author']) ? $_REQUEST['parent_author'] : '');
-$terms  = urlencode(isset($_REQUEST['terms']) ? $_REQUEST['terms'] : '');
-$page   = urlencode(isset($_REQUEST['page']) ? $_REQUEST['page'] : '1');
-
-$_SERVER['PHP_SELF'] = "/service/json.php/ClassicService.search/$terms/$author/$parent/$page/";
-chdir('../service/');
-include '../service/json.php';
+$author = isset($_REQUEST['author']) ? $_REQUEST['author'] : '';
+$parent = isset($_REQUEST['parent_author']) ? $_REQUEST['parent_author'] : '';
+$terms  = isset($_REQUEST['terms']) ? $_REQUEST['terms'] : '';
+$page   = isset($_REQUEST['page']) ? $_REQUEST['page'] : '1';
+die(json_encode(ClassicAdapter::search($terms, $author, $parent, $page)));
