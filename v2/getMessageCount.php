3cd44fb5d51143b68acab7299e53862559d5a97a
Brian Luft <brian@electroly.com>
2015-01-08 20:23:38 -0400

    Implement /v2/getMessageCount.

---
+++ v2/getMessageCount.php
@@ -0,0 +1,33 @@
+<?
+# WinChatty Server
+# Copyright (C) 2015 Brian Luft
+# 
+# This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public 
+# License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later 
+# version.
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+# details.
+# 
+# You should have received a copy of the GNU General Public License along with this program; if not, write to the Free 
+# Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+require_once 'Global.php';
+$pg = nsc_initJsonPost();
+$username = nsc_postArg('username', 'STR');
+$password = nsc_postArg('password', 'STR');
+
+try
+{
+   $m = MessageParser()->getMessages('inbox', $username, $password, 1, 0);
+}
+catch (Exception $e)
+{
+   nsc_handleException($e);
+}
+
+echo json_encode(array(
+   'total' => intval($m['total']),
+   'unread' => intval($m['unread'])
+));
