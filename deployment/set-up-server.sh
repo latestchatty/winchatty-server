ae6594a9236341f5e808b8a727dc9cdbd3c30b39
Brian Luft <brian@electroly.com>
2015-02-13 03:58:24 -0400

    Fix variable name collision.

--- deployment/set-up-server.sh
+++ deployment/set-up-server.sh
@@ -11,12 +11,12 @@
 # Restoring the complete database can take several hours.  The sample database takes only a few minutes.
 #
 # Installation instructions (as root):
-#   USERNAME=(name of the new unix user that will own all site files and processes)
-#   SHACK_USERNAME=(shacknews username)
-#   SHACK_PASSWORD=(shacknews password)
-#   DUMP_FILE=(filename of the sql dump to use, see above)
+#   export OWNER=(name of the new unix user that will own all site files and processes)
+#   export SHACK_USERNAME=(shacknews username)
+#   export SHACK_PASSWORD=(shacknews password)
+#   export DUMP_FILE=(filename of the sql dump to use, see above)
 #   curl https://raw.githubusercontent.com/electroly/winchatty-server/master/deployment/set-up-server.sh | bash
-#   passwd $USERNAME
+#   passwd $OWNER
 #   reboot
 #
 # To use your newly provisioned server, on your local computer edit the hosts file (/etc/hosts or
@@ -25,7 +25,7 @@
 # easily flip back and forth between the real winchatty.com and your instance just by editing the hosts file.
 
 if (( EUID != 0 )); then echo "Must be root."; exit 1; fi
-if [ -z "$USERNAME" ]; then echo "Missing USER."; exit 1; fi
+if [ -z "$OWNER" ]; then echo "Missing USER."; exit 1; fi
 if [ -z "$SHACK_USERNAME" ]; then echo "Missing SHACK_USERNAME."; exit 1; fi
 if [ -z "$SHACK_PASSWORD" ]; then echo "Missing SHACK_PASSWORD."; exit 1; fi
 if [ -z "$DUMP_FILE" ]; then echo "Missing DUMP_FILE."; exit 1; fi
@@ -36,20 +36,20 @@ set -x
 apt-get update
 apt-get -y upgrade
 apt-get -y install apache2 postgresql pgbouncer php5 php5-pgsql php5-cli php-apc php-pear libapache2-mod-php5 nodejs \
-   nodejs-legacy npm build-essential zip unzip git s3cmd htop
+   nodejs-legacy npm build-essential zip unzip git s3cmd htop pv mc
 
 echo "America/Chicago" > /etc/timezone
 dpkg-reconfigure --frontend noninteractive tzdata
 
-useradd -g www-data -m $USERNAME
+useradd -g www-data -m $OWNER
 
 mkdir /home/chatty
-chown $USERNAME:www-data /home/chatty
+chown $OWNER:www-data /home/chatty
 
 pushd /home/chatty
-sudo -H -u $USERNAME git clone --recursive https://github.com/electroly/winchatty-server.git backend
-sudo -u $USERNAME mkdir backend-data
-sudo -H -u $USERNAME git clone --recursive https://github.com/NiXXeD/chatty.git frontend
+sudo -H -u $OWNER git clone --recursive https://github.com/electroly/winchatty-server.git backend
+sudo -u $OWNER mkdir backend-data
+sudo -H -u $OWNER git clone --recursive https://github.com/NiXXeD/chatty.git frontend
 chmod 744 backend/*.sh
 popd
 
@@ -57,20 +57,20 @@ pushd /home/chatty/backend/include
 echo "<?" > ConfigUserPass.php
 echo "define('WINCHATTY_USERNAME', '$SHACK_USERNAME');" >> ConfigUserPass.php
 echo "define('WINCHATTY_PASSWORD', '$SHACK_PASSWORD');" >> ConfigUserPass.php
-chown $USERNAME:www-data ConfigUserPass.php
+chown $OWNER:www-data ConfigUserPass.php
 popd
 
 pushd /home/chatty/backend/push-server
-sudo -H -u $USERNAME npm install
+sudo -H -u $OWNER npm install
 popd
 
 mkdir /mnt/websites
-chown $USERNAME:www-data /mnt/websites
-sudo -u $USERNAME ln -s /home/chatty/backend /mnt/websites/winchatty.com
+chown $OWNER:www-data /mnt/websites
+sudo -u $OWNER ln -s /home/chatty/backend /mnt/websites/winchatty.com
 
 mkdir /mnt/ssd
-chown $USERNAME:www-data /mnt/ssd
-sudo -u $USERNAME ln -s /home/chatty/backend-data /mnt/ssd/ChattyIndex
+chown $OWNER:www-data /mnt/ssd
+sudo -u $OWNER ln -s /home/chatty/backend-data /mnt/ssd/ChattyIndex
 
 pushd /home/chatty/backend/deployment
 cp -f pgbouncer/pgbouncer.ini /etc/pgbouncer/
@@ -80,8 +80,8 @@ cp -f apache/apache2.conf /etc/apache2/
 cp -f apache/ports.conf /etc/apache2/
 cp -f php/php-apache.ini /etc/php5/apache2/php.ini
 cp -f php/php-cli.ini /etc/php5/cli/php.ini
-sed "s/USERNAME/$USERNAME/g" upstart/winchatty-indexer.conf > /etc/init/winchatty-indexer.conf
-sed "s/USERNAME/$USERNAME/g" upstart/winchatty-push-server.conf > /etc/init/winchatty-push-server.conf
+sed "s/USERNAME/$OWNER/g" upstart/winchatty-indexer.conf > /etc/init/winchatty-indexer.conf
+sed "s/USERNAME/$OWNER/g" upstart/winchatty-push-server.conf > /etc/init/winchatty-push-server.conf
 popd
 
 sudo -u postgres psql --command "CREATE USER nusearch WITH PASSWORD 'nusearch';"
