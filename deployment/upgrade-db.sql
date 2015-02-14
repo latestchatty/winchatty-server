54d5375a2836099811dc0c54501b737ae4e49b20
Brian Luft <brian@electroly.com>
2015-02-14 17:25:34 -0400

    Database version upgrade script.

---
+++ deployment/upgrade-db.sql
@@ -0,0 +1,27 @@
+-- This script will migrate your database to the current version.
+-- The version number is stored as a comment on the 'indexer' table.
+-- 
+-- Instructions: (as postgres or a user with permission to access the db)
+--   psql -f upgrade-db.sql chatty
+
+DO $$
+   BEGIN
+
+   RAISE NOTICE 'Old version: %', obj_description('indexer'::regclass, 'pg_class');
+
+   IF obj_description('indexer'::regclass, 'pg_class') IS NULL THEN
+      RAISE NOTICE 'Upgrading to version 1...';
+      CREATE TABLE post_lols (
+         post_id INTEGER NOT NULL,
+         tag TEXT NOT NULL,
+         count INTEGER NOT NULL,
+         PRIMARY KEY (post_id, tag)
+      );
+      CREATE INDEX idx_post_lols_post_id ON post_lols (post_id);
+      COMMENT ON TABLE indexer IS '1';
+   END IF;
+
+   RAISE NOTICE 'New version: %', obj_description('indexer'::regclass, 'pg_class');
+
+   END;
+$$ LANGUAGE plpgsql;
