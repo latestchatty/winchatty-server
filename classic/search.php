160cbfaec3d4ad01856d667eabfadf0bfec8fa11
Brian Luft <brian@electroly.com>
2016-01-18 19:27:54 -0600

    Revert "Use NuSearch for v1 and v2 search APIs."
    This reverts commit 836289574534e5130e6194b80303a98957b71210.

--- classic/search.php
+++ classic/search.php
@@ -7,4 +7,4 @@ $author = isset($_REQUEST['author']) ? $_REQUEST['author'] : '';
 $parent = isset($_REQUEST['parent_author']) ? $_REQUEST['parent_author'] : '';
 $terms  = isset($_REQUEST['terms']) ? $_REQUEST['terms'] : '';
 $page   = isset($_REQUEST['page']) ? $_REQUEST['page'] : '1';
-die(json_encode(ClassicAdapter::nusearch($terms, $author, $parent, $page)));
+die(json_encode(ClassicAdapter::search($terms, $author, $parent, $page)));
--- v2/search.php
+++ v2/search.php
@@ -25,5 +25,77 @@ $offset = nsc_getArg('offset', 'INT?', 0);
 $limit = nsc_getArg('limit', 'INT?,500', 35);
 $oldestFirst = nsc_getArg('oldestFirst', 'BIT?', false);
 
-$posts = nsc_search($pg, $terms, $author, $parentAuthor, $category, $offset, $limit, $oldestFirst);
-echo json_encode(array('posts' => $posts));
+if (empty($terms) && empty($author) && empty($parentAuthor))
+   nsc_die('ERR_SERVER', '(Temporary) A search term, author, or parent author query is required.');
+
+#$posts = nsc_search($pg, $terms, $author, $parentAuthor, $category, $offset, $limit, $oldestFirst);
+
+$perPage = 15;
+$startingPage = floor((float)$offset / (float)$perPage);
+$startingPageFirstIndex = $offset - ($startingPage * $perPage);
+$endingPage = floor((float)($offset + $limit - 1) / (float)$perPage);
+$endingPageLastIndex = ($offset + $limit - 1) - ($endingPage * $perPage);
+
+$startingPage++;
+$endingPage++;
+
+$postIds = array();
+for ($page = $startingPage; $page <= $endingPage; $page++)
+{
+   try
+   {
+      $pageResults = SearchParser()->search($terms, $author, $parentAuthor, $category, $page, $oldestFirst);
+   }
+   catch (Exception $e)
+   {
+      nsc_handleException($e);
+   }
+
+   if (empty($pageResults))
+      break;
+   
+   foreach ($pageResults as $i => $result)
+   {
+      if ($page == $startingPage && $i < $startingPageFirstIndex)
+      {
+         # Nothing
+      }
+      else if ($page == $endingPage && $i > $endingPageLastIndex)
+      {
+         break;
+      }
+      else
+      {
+         $postIds[] = intval($result['id']);
+      }
+   }
+}
+
+if (empty($postIds))
+   die(json_encode(array('posts' => array())));
+
+$posts = nsc_getPosts($pg, $postIds);
+$dict = array();
+foreach ($posts as $post)
+   $dict[$post['id']] = $post;
+
+$searchResults = array();
+foreach ($postIds as $postId)
+{
+   if (isset($dict[$postId]))
+      $searchResults[] = $dict[$postId];
+   else
+      $searchResults[] = array(
+         'id' => $postId,
+         'threadId' => 0,
+         'parentId' => 0,
+         'author' => 'Duke Nuked',
+         'category' => 'ontopic',
+         'date' => nsc_date(strtotime('1980-01-01 Midnight UTC')),
+         'body' => '<span class="jt_red"><b>* N U K E D *</b></span>',
+         'lols' => array()
+      );
+}
+
+echo json_encode(array('posts' => $searchResults));
+
