fc046ec7ced09771186c561174e38439cccae581
Brian Luft <brian@electroly.com>
2019-02-14 22:08:04 -0400

    Accept a list of post IDs in /v2/clientData/markPost

--- v2/clientData/markPost.php
+++ v2/clientData/markPost.php
@@ -18,28 +18,30 @@
 require_once 'Global.php';
 $pg = nsc_initJsonPost();
 $username = nsc_postArg('username', 'STR,50');
-$postId = nsc_postArg('postId', 'INT');
+$postIds = nsc_postArg('postId', 'INT+');
 $type = nsc_postarg('type', 'MPT');
 $shackerId = nsc_getShackerId($pg, $username);
 
 nsc_execute($pg, 'BEGIN', array());
 
-# Clear any existing mark.
-nsc_execute($pg, 
-   'DELETE FROM shacker_marked_post WHERE shacker_id = $1 AND post_id = $2', 
-   array($shackerId, $postId));
+foreach ($postIds as $postId) {
+   # Clear any existing mark.
+   nsc_execute($pg, 
+      'DELETE FROM shacker_marked_post WHERE shacker_id = $1 AND post_id = $2', 
+      array($shackerId, $postId));
 
-# Make sure the post exists.
-$existingPostId = nsc_selectValueOrFalse($pg, 'SELECT id FROM post WHERE id = $1', array($postId));
-if ($existingPostId === false)
-   nsc_die('ERR_POST_DOES_NOT_EXIST', 'The specified post does not exist.');
+   # Make sure the post exists.
+   $existingPostId = nsc_selectValueOrFalse($pg, 'SELECT id FROM post WHERE id = $1', array($postId));
+   if ($existingPostId === false)
+      nsc_die('ERR_POST_DOES_NOT_EXIST', 'The specified post does not exist.');
 
-# Add a new mark, if necessary.
-if ($type == 'pinned' || $type == 'collapsed')
-{
-   nsc_execute($pg,
-      'INSERT INTO shacker_marked_post (shacker_id, post_id, mark_type) VALUES ($1, $2, $3)',
-      array($shackerId, $postId, nsc_markTypeStringToInt($type)));
+   # Add a new mark, if necessary.
+   if ($type == 'pinned' || $type == 'collapsed')
+   {
+      nsc_execute($pg,
+         'INSERT INTO shacker_marked_post (shacker_id, post_id, mark_type) VALUES ($1, $2, $3)',
+         array($shackerId, $postId, nsc_markTypeStringToInt($type)));
+   }
 }
 
 nsc_execute($pg, 'COMMIT', array());
