516718362e617e2d842cd27b9cd64d423acaae78
Brian Luft <electroly@users.noreply.github.com>
2019-03-14 17:53:14 -0400

    💩 part 2

--- include/ChattyParser.php
+++ include/ChattyParser.php
@@ -15,6 +15,56 @@
 // OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+function entities( $string ) {
+    $stringBuilder = "";
+    $offset = 0;
+
+    if ( empty( $string ) ) {
+        return "";
+    }
+
+    while ( $offset >= 0 ) {
+        $decValue = ordutf8( $string, $offset );
+        $char = unichr($decValue);
+
+        $htmlEntited = htmlentities( $char );
+        if( $char != $htmlEntited ){
+            $stringBuilder .= $htmlEntited;
+        } elseif( $decValue >= 128 ){
+            $stringBuilder .= "&#" . $decValue . ";";
+        } else {
+            $stringBuilder .= $char;
+        }
+    }
+
+    return $stringBuilder;
+}
+
+// source - http://php.net/manual/en/function.ord.php#109812
+function ordutf8($string, &$offset) {
+    $code = ord(substr($string, $offset,1));
+    if ($code >= 128) {        //otherwise 0xxxxxxx
+        if ($code < 224) $bytesnumber = 2;                //110xxxxx
+        else if ($code < 240) $bytesnumber = 3;        //1110xxxx
+        else if ($code < 248) $bytesnumber = 4;    //11110xxx
+        $codetemp = $code - 192 - ($bytesnumber > 2 ? 32 : 0) - ($bytesnumber > 3 ? 16 : 0);
+        for ($i = 2; $i <= $bytesnumber; $i++) {
+            $offset ++;
+            $code2 = ord(substr($string, $offset, 1)) - 128;        //10xxxxxx
+            $codetemp = $codetemp*64 + $code2;
+        }
+        $code = $codetemp;
+    }
+    $offset += 1;
+    if ($offset >= strlen($string)) $offset = -1;
+    return $code;
+}
+
+// source - http://php.net/manual/en/function.chr.php#88611
+function unichr($u) {
+    return mb_convert_encoding('&#' . intval($u) . ';', 'UTF-8', 'HTML-ENTITIES');
+}
+
 class ChattyParser extends Parser
 {
    public function getStory($storyID, $page)
@@ -96,7 +146,7 @@ class ChattyParser extends Parser
          'content_id' => strval($contentID), 
          'page' => '', 
          'parent_url' => '/chatty',
-         'body' => $body);
+         'body' => entities($body));
 
       $retVal = $this->userDownload($postURL, $username, $password, $postArgs);
       
